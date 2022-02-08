d9ff7f4b3fb40fe24bc449c0f6bcd553eaa3d12b
OGBlackDiamond <84690305+OGBlackDiamond@users.noreply.github.com>
2022-02-07 17:43:47 -0800

    Gave the code some meat on its bones
    Simply made more code such that there is a visible UI, and the code runs functionally.

--- Main.py
+++ Main.py
@@ -12,6 +12,8 @@ pygame.display.set_caption("Create Task Entry")
 #Defining and setting the window size
 WIDTH, HEIGHT = 320, 450
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+#Gets the background image. I made it myself
+BACKGROUND = pygame.image.load(os.path.join('Images', 'Background.png'))
 #Setting the speed (Frames Per Second) at which the app runs
 FPS = 60
 #Timer variable to keep track of the current FPS
@@ -23,16 +25,48 @@ timer = 0
 class Player:
     #Initializing the class variables
     def __init__(self):
-        self.playerx, self.playery = 0, 0
-        self.hurtbox = pygame.Rect(self.playerx, self.playery, 0, 0)
+        #Setting variables for the player
+        self.playerx, self.playery = 145, 400
+        self.player_sizex, self.player_sizey = 30, 32
+        self.hurtbox = pygame.Rect(self.playerx, self.playery, self.player_sizex, self.player_sizey)
+        #Getting the player (spaceship) image
+        #All credit to the image used goes to:
+        #https://galaxian.fandom.com/wiki/Gyaraga
+        self.raw_player_image = pygame.image.load(os.path.join("Images", "Galaga Ship.png"))
+        self.player_image = pygame.transform.scale(self.raw_player_image, (self.player_sizex, self.player_sizey))
+
+    def draw(self):
+        WIN.blit(self.player_image, (self.playerx, self.playery))
+
+        
         
 #Defining an enemy class so we can make more than one very easily
 class Enemy:
     #Initializing the class variables
     def __init__(self):
-        #Some code here
-        self.bazinga = "beans"
+        #Setting variables for the player
+        self.enemyx, self.enemyy = 145, 150
+        self.enemy_sizex, self.enemy_sizey = 30, 32
+        self.hurtbox = pygame.Rect(self.enemyx, self.enemyy, self.enemy_sizex, self.enemy_sizey)
+        #Getting the enemy (alien) image
+        #All credit to the image used goes to:
+        #https://supersmashbros.fandom.com/wiki/Boss_Galaga
+        self.raw_enemy_image = pygame.image.load(os.path.join("Images", "Galaga Enemy.png"))
+        self.enemy_image = pygame.transform.scale(self.raw_enemy_image, (self.enemy_sizex, self.enemy_sizey))
 
+    def draw(self):
+        WIN.blit(self.enemy_image, (self.enemyx, self.enemyy))
+
+#Class instance definition
+player1 = Player() 
+enemies = [Enemy()]
+#Draws all of the necessary elements on the screen
+def draw():
+    WIN.blit(BACKGROUND, (0, 0))
+    player1.draw()
+    enemies[0].draw()
+    #Updates the screen
+    pygame.display.update()  
 
 #Making the game loop
 while True:
@@ -40,12 +74,15 @@ while True:
     clock = pygame.time.Clock()
     clock.tick(FPS)
     timer += 1
-    if timer >= 60:
+    if timer == 60:
         timer = 0
+
 
     #Getting user input
     keys_pressed = pygame.key.get_pressed()
 
+    #Main game loop
+    draw()
 
     #Lets the code stop running when the window is closed
     for event in pygame.event.get():
